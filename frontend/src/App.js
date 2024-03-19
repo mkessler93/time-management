eb613fac93a36272d3aa0b37255942f7ddc57176
Michael Kessler <michael.kessler@fau.de>
2024-03-19 19:56:30 +0100

    chore: add frontend and backend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Timer from './Timer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <Timer />
+      </main>
+    </div>
+  );
+}
+
+export default App;
